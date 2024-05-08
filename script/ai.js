cc645687bb9fc0e4d41e0c0a62a83cb8ef758793
frenchmangigo <160458884+frenchmangigo@users.noreply.github.com>
2024-05-09 06:21:44 +0800

    Update ai.js

--- script/ai.js
+++ script/ai.js
@@ -5,34 +5,34 @@ module.exports.config = {
   version: '1.0.0',
   role: 0,
   hasPrefix: false,
-  aliases: ['gpt', 'openai'],
-  description: "An AI command powered by GPT-4",
-  usage: "ai [prompt]",
-  credits: 'Developer: https://www.facebook.com/frenchclarence.mangigo.9',
+  aliases: ['snow', 'ai'],
+  description: "An AI command powered by Snowflakes AI",
+  usage: "snowflakes [prompt]",
+  credits: 'churchill',
   cooldown: 3,
 };
 
-module.exports.run = async function({
-  api,
-  event,
-  args
-}) {
+module.exports.run = async function({ api, event, args }) {
   const input = args.join(' ');
+  
   if (!input) {
-    api.sendMessage(`𝑯𝑬𝑳𝑳𝑶 𝑰𝑴 𝑨𝑰 ✨ 
+    api.sendMessage(`𝑯𝑬𝑳𝑳𝑶! 𝑰'𝑴 𝑨 𝑺𝑵𝑶𝑾𝑭𝑳𝑨𝑲𝑬𝑺 𝑨𝑰 𝑩𝑶𝑻 ✨ 
 
 ━━━━━━━━━━━━━━━
 
- 𝑷𝑳𝑬𝑨𝑺𝑬 𝑷𝑹𝑶𝑽𝑰𝑫𝑬 𝑨 𝑸𝑼𝑬𝑺𝑻𝑰𝑶𝑵/𝑸𝑼𝑬𝑹𝒀`, event.threadID, event.messageID);
+ 𝐛𝐚𝐭 𝐩𝐮𝐫𝐨 𝐤𝐚 𝐀𝐈 𝐭𝐚𝐧𝐠𝐚, 𝐦𝐚𝐠 𝐭𝐚𝐧𝐨𝐧𝐠 𝐤𝐚𝐧𝐚!`, event.threadID, event.messageID);
     return;
   }
-  api.sendMessage(`🔍𝙎𝙚𝙖𝙧𝙘𝙝𝙞𝙣𝙜 𝙋𝙡𝙚𝙖𝙨𝙚 𝙈𝙖𝙜 𝙖𝙣𝙩𝙖𝙮 𝙠𝙖!....
-━━━━━━━━━━━━━━━━━━\n\n "${input}"`, event.threadID, event.messageID);
+  
+  api.sendMessage(`🔍Searching for Snowflakes AI response....`, event.threadID, event.messageID);
+  
   try {
-    const { data } = await axios.get(`https://openaikey-x20f.onrender.com/api?prompt=${encodeURIComponent(input)}`);
-    const response = data.response;
-    const credits = '\n𝐜𝐫𝐞𝐝𝐢𝐭𝐬: https://www.facebook.com/frenchclarence.mangigo.9';
-    api.sendMessage(response + credits, event.threadID, event.messageID);
+    const { data } = await axios.get(`https://hashier-api-snowflake.vercel.app/api/snowflake?ask=${encodeURIComponent(input)}`);
+    if (data.response) {
+      api.sendMessage(data.response + "\n\nhttps://web.facebook.com/frenchclarence.mangigo.9", event.threadID, event.messageID);
+    } else {
+      api.sendMessage('No response found.', event.threadID, event.messageID);
+    }
   } catch (error) {
     api.sendMessage('An error occurred while processing your request.', event.threadID, event.messageID);
   }
