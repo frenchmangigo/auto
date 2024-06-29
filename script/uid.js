f584f3b71f8c70ea19998bc25269bac9eda68683
frenchmangigo <160458884+frenchmangigo@users.noreply.github.com>
2024-06-29 21:33:58 +0800

    Update uid.js

--- script/uid.js
+++ script/uid.js
@@ -3,7 +3,7 @@ module.exports.config = {
 	role: 0,
 	credits: "Mirai Team",
 	description: "Get the user's Facebook UID.",
-	hasPrefix: false,
+	hasPrefix: true,
 	usages: "{p}uid {p}uid @mention",
 	cooldown: 5,
 	aliases: ["id","ui"]
