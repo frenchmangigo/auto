1589af6f08834ed2c53b96cc48d251a5e7d6c517
frenchmangigo <160458884+frenchmangigo@users.noreply.github.com>
2024-06-29 22:14:24 +0800

    Update prefix.js

--- script/prefix.js
+++ script/prefix.js
@@ -36,7 +36,7 @@ module.exports.run = function ({ api, event, prefix, admin }) {
     // Sending the message along with the attachment
     api.sendMessage(
         {
-            body: `Yo, my prefix is [ 𓆩 ${prefix} 𓆪 ]\n\n𝗦𝗢𝗠𝗘 𝗖𝗢𝗠𝗠𝗔𝗡𝗗𝗦 𝗧𝗛𝗔𝗧 𝗠𝗔𝗬 𝗛𝗘𝗟𝗣 𝗬𝗢𝗨:\n➥ ${prefix}help [number of page] -> see commands\n➥ ${prefix}sim [message] -> talk to bot\n➥ ${prefix}callad [message] -> report any problem encountered\n➥ ${prefix}help [command] -> information and usage of command\n\nHave fun using it, enjoy! ❤️\nBot Developer: https://www.facebook.com/Churchill.Dev4100`,
+            body: `Yo, my prefix is [ 𓆩 ${prefix} 𓆪 ]`,
             attachment: fs.createReadStream(__dirname + "/cache2/prefix.jpeg")
         },
         threadID,
